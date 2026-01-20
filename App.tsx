6ab6739e1b660923c5464d0d1356942244342741
Facundo Lozano <facufabi@hotmail.com>
2026-01-20 20:12:15 -0300

    123

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import { supabase } from './lib/supabase';
+
 import React, { useState } from 'react';
 import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -19,7 +21,6 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import UserProfile from './pages/UserProfile';
 import Help from './pages/Help';
-import { supabase } from './lib/supabase';
 import { ToastMessage } from './types';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
